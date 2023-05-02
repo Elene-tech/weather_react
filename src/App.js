d900f9860f99f86e011e0eddd18b2aaadb781133
Olena Kurilets <elenakurilets@gmail.com>
2023-05-02 09:41:24 +0200

    Add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Olena Kurilets and is{' '}
+        <a
+          href="https://github.com/Elene-tech/weather_react/tree/master"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
